ac37c45de60fc1d097e142b033fd2e3f5e0b7512
John Glover <john.c.glover@nuim.ie>
2012-01-16 16:59:10 +0000

    [controllers] Add fixed values for geographical coverage

--- ckanext/ecportal/field_values.py
+++ ckanext/ecportal/field_values.py
@@ -117,6 +117,38 @@ publishers = [
     ('research executive agency', 'Research Executive Agency')
 ]
 
+geographical_coverage = [
+    ('', ''),
+    ('EU27', 'European Union')
+    ('BE', 'Belgium')
+    ('BG', 'Bulgaria')
+    ('CZ', 'Czech Republic')
+    ('DK', 'Denmark')
+    ('DE', 'Germany')
+    ('EE', 'Estonia')
+    ('IE', 'Ireland')
+    ('EL', 'Greece')
+    ('ES', 'Spain')
+    ('FR', 'France')
+    ('IT', 'Italy')
+    ('CY', 'Cyprus')
+    ('LV', 'Latvia')
+    ('LT', 'Lithuania')
+    ('LU', 'Luxembourg')
+    ('HU', 'Hungary')
+    ('MT', 'Malta')
+    ('NL', 'Netherlands')
+    ('AT', 'Austria')
+    ('PL', 'Poland')
+    ('PT', 'Portugal')
+    ('RO', 'Romania')
+    ('SI', 'Slovenia')
+    ('SK', 'Slovakia')
+    ('FI', 'Finland')
+    ('SE', 'Sweden')
+    ('UK', 'United Kingdom')
+]
+
 geographic_granularity = [
     ('', ''),
     ('national', 'national'),
