478b59b8431036d35ac5c0169063e39d48bfe955
John Glover <j@johnglover.net>
2012-01-30 15:08:17 +0000

    [templates, validators] Update date validator to only accept YYYY-MM-DD, YYYY-MM and YYYY

--- ckanext/ecportal/validators.py
+++ ckanext/ecportal/validators.py
@@ -1,18 +1,71 @@
+import re
 from pylons.i18n import _
 from ckan.lib.field_types import DateType, DateConvertError
 from ckan.lib.navl.dictization_functions import Invalid, missing, unflatten
 from field_types import GeoCoverageType
 
+# parse_timedate function is similar to the one in ckan.lib.field_types.DateType
+# changes:
+# - doesn't accept a time value
+# - different message passed to DateConvertError exception 
+class ECPortalDateType(DateType):
+    @classmethod
+    def parse_timedate(cls, timedate_str, format_type):
+        '''Takes a timedate and returns a dictionary of the fields.
+        * Little validation is done.
+        * If it can\'t understand the layout it raises DateConvertError
+        '''
+        assert format_type in cls.format_types
+        if not hasattr(cls, 'matchers'):
+            # build up a list of re matches for the different
+            # acceptable ways of expressing the time and date
+            cls.matchers = {}
+            cls.readable_formats = {}
+            for format_type_ in cls.format_types:
+                finished_regexps = []
+                readable_formats = [] # analogous to the regexps, but human readable
+                year_re = '(?P<%s>\d{2,4})'
+                month_re = '(?P<%s>\w+)'
+                two_digit_decimal_re = '(?P<%s>\d{1,2})'
+                date_field_re = {'year':year_re % 'year',
+                                 'month':month_re % 'month',
+                                 'day':two_digit_decimal_re % 'day'}
+                date_fields = list(cls.date_fields_order[format_type_])
+                for how_specific in ('day', 'month', 'year'):
+                    date_sep_re = '[%s]' % cls.parsing_separators['date']
+                    date_sep_readable = cls.default_separators[format_type_]['date']
+                    date_field_regexps = [date_field_re[field] for field in date_fields]
+                    date_field_readable = [cls.datetime_fields[field][cls.datetime_fields_indexes['format_code']] for field in date_fields]
+                    date_re = date_sep_re.join(date_field_regexps)
+                    date_readable = date_sep_readable.join(date_field_readable)
+                    finished_regexps.append(date_re)
+                    readable_formats.append(date_readable)
+                    date_fields.remove(how_specific)
+                full_date_re = finished_regexps[0]
+                full_date_readable = readable_formats[0]
+                cls.matchers[format_type_] = [re.compile('^%s$' % regexp) for regexp in finished_regexps]
+                cls.readable_formats[format_type_] = readable_formats
+        for index, matcher in enumerate(cls.matchers[format_type]):
+            match = matcher.match(timedate_str)
+            if match:
+                timedate_dict = match.groupdict()
+                timedate_dict = cls.int_timedate(timedate_dict)
+                timedate_dict['readable_format'] = cls.readable_formats[format_type][index]
+                return timedate_dict
+        else:
+            acceptable_formats = ', '.join(["'%s'" % format_ for format_ in cls.readable_formats[format_type]])
+            raise DateConvertError("Unknown date format: '%s'. Acceptable formats: %s" % (timedate_str, acceptable_formats))
+
 def ecportal_date_to_db(value, context):
     if not value:
         return
     try:
-        timedate_dict = DateType.parse_timedate(value, 'db')
+        timedate_dict = ECPortalDateType.parse_timedate(value, 'db')
     except DateConvertError, e:
         # Cannot parse
         raise Invalid(str(e))
     try:
-        value = DateType.format(timedate_dict, 'db')
+        value = ECPortalDateType.format(timedate_dict, 'db')
     except DateConvertError, e:
         # Values out of range
         raise Invalid(str(e))
