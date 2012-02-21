b24374cb4ab10f7fd587d4878986d3c756e5abb8
John Glover <j@johnglover.net>
2012-02-21 17:13:26 +0000

    [1817][commands] update publisher importer. Currently creates groups, but not hierarchy.

--- ckanext/ecportal/commands.py
+++ ckanext/ecportal/commands.py
@@ -2,7 +2,6 @@ import os
 import json
 from ckan import model
 from ckan.lib.cli import CkanCommand
-from ckan.logic import get_action, NotFound
 
 import logging
 log = logging.getLogger()
@@ -53,8 +52,49 @@ class ECPortalCommand(CkanCommand):
 
     def import_publishers(self, translations, structure):
         '''
+        Create publisher groups based on translations and structure JSON objects.
         '''
-        user = get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
-        context = {'model': model, 'session': model.Session, 'user': user['name']}
-        print translations
-        print structure
+        # get group names and title translations
+        groups = {}
+        for group in translations['results']['bindings']:
+            translation = {}
+            name_uri = group['s']['value']
+            group_name = name_uri.split('/')[-1].lower()
+            lang_uri = group['lang']['value']
+            lang_name = lang_uri.split('/')[-1].lower()
+            translation[lang_name] = group['f']['value']
+            if not group_name in groups:
+                groups[group_name] = {}
+                groups[group_name]['titles'] = []
+            groups[group_name]['titles'].append(translation)
+
+        # get list of child groups for each group
+        for group in groups:
+            groups[group]['children'] = []
+
+            for relationship in structure['results']['bindings']:
+                parent = relationship['ch']['value'].split('/')[-1].lower()
+                if parent == group:
+                    child = relationship['s']['value'].split('/')[-1].lower()
+                    groups[group]['children'].append(child)
+
+        # create CKAN groups
+        rev = model.repo.new_revision()
+        rev.message = u'Importing EC Portal Publisher Info'
+        for group in groups:
+            g = model.Group.get(group)
+            if not g:
+                # use the english title if we have one
+                title_langs = [k.keys()[0] for k in groups[group]['titles']]
+                if 'eng' in title_langs:
+                    group_title = [t['eng'] for t in groups[group]['titles'] if t.get('eng')][0]
+                else:
+                    group_title = groups[group]['titles'][0][title_langs[0]]
+
+                g = model.Group(name=group, title=group_title, type=u'publisher')
+                model.Session.add(g)
+            groups[group]['id'] = g.id
+
+        # TODO: setup group heirarchy
+
+        model.repo.commit_and_remove()
