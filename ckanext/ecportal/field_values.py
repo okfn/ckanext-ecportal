d4f5951ed8c8149fdf5fd4f0e5d9b4fc37ca34a1
John Glover <j@johnglover.net>
2012-05-01 16:22:57 +0100

    [2343][field_values] remove type_of_dataset dict (is in JSON now)

--- ckanext/ecportal/field_values.py
+++ ckanext/ecportal/field_values.py
@@ -1,18 +1,3 @@
-type_of_dataset = {
-    u'http://purl.org/adms/assettype/CoreComponent': u'Core Component',
-    u'http://purl.org/adms/assettype/Ontology': u'Ontology',
-    u'http://purl.org/adms/assettype/DomainModel': u'Domain Model',
-    u'http://purl.org/adms/assettype/Schema': u'Schema',
-    u'http://purl.org/adms/assettype/InformationExchangePackageDescription': u'Information Exchange Package Description',
-    u'http://purl.org/adms/assettype/Thesaurus': u'Thesaurus',
-    u'http://purl.org/adms/assettype/Taxonomy': u'Taxonomy',
-    u'http://purl.org/adms/assettype/CodeList': u'CodeList',
-    u'http://purl.org/adms/assettype/NameAuthorityList': u'Name Authority List',
-    u'http://purl.org/adms/assettype/Mapping': u'Mapping',
-    u'http://purl.org/adms/assettype/SyntaxEncodingScheme': u'Syntax Encoding Scheme',
-    u'http://purl.org/adms/assettype/ServiceDescription': u'Service Description'
-}
-
 interoperability_levels = [
     (u'', u''),
     (u'http://purl.org/adms/interoperabilitylevel/Political', u'Political'),
