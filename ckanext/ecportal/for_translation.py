2b816433bc73a8bcc91e7a0e4e5413d6dd157159
kindly <kindly@gmail.com>
2012-05-23 15:59:10 +0100

    add extra translation terms to the pot

---
+++ ckanext/ecportal/for_translation.py
@@ -0,0 +1,51 @@
+from ckan.lib.base import _
+
+def terms_for_traslation():
+
+    #Distribution type
+    _('Feed')
+    _('Web Service')
+    _('Download')
+
+    #Documentation type
+    _("Main Documentation")
+    _("Related Documentation")
+    _("Related Web Page")
+
+    #Dataset type
+    _("Code List")
+    _("Core Component")
+    _("Domain Model")
+    _("Information Exchange Package Description")
+    _("Mapping")
+    _("Name Authority List")
+    _("Ontology")
+    _("Schema")
+    _("Service Description")
+    _("Statistical")
+    _("Syntax Encoding Scheme")
+    _("Taxonomy")
+    _("Thesaurus")
+
+    #Status
+    _("Completed")
+    _("Deprecated")
+    _("Under Development")
+    _("Withdrawn")
+
+    #Accural Periodicy
+    _("Never")
+    _("Discontinued")
+    _("Annual")
+    _("Quarterly")
+    _("Monthly")
+    _("Other - please specify")
+
+    #Interoperability level
+    _("Never")
+    _("Discontinued")
+    _("Annual")
+    _("Quarterly")
+    _("Monthly")
+    _("Other - please specify")
+
