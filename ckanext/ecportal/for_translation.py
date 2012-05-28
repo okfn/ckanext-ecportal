90b3b5cee2dbfa2eb79ae302c5bc77022d0864ce
John Glover <j@johnglover.net>
2012-05-28 16:16:14 +0100

    Update strings for translation

--- ckanext/ecportal/for_translation.py
+++ ckanext/ecportal/for_translation.py
@@ -1,51 +1,104 @@
 from ckan.lib.base import _
 
-def terms_for_traslation():
 
-    #Distribution type
+def terms_for_traslation():
+    # distribution type
     _('Feed')
     _('Web Service')
     _('Download')
 
-    #Documentation type
-    _("Main Documentation")
-    _("Related Documentation")
-    _("Related Web Page")
+    # documentation type
+    _('Main Documentation')
+    _('Related Documentation')
+    _('Related Web Page')
 
-    #Dataset type
-    _("Code List")
-    _("Core Component")
-    _("Domain Model")
-    _("Information Exchange Package Description")
-    _("Mapping")
-    _("Name Authority List")
-    _("Ontology")
-    _("Schema")
-    _("Service Description")
-    _("Statistical")
-    _("Syntax Encoding Scheme")
-    _("Taxonomy")
-    _("Thesaurus")
+    # dataset type
+    _('Code List')
+    _('Core Component')
+    _('Domain Model')
+    _('Information Exchange Package Description')
+    _('Mapping')
+    _('Name Authority List')
+    _('Ontology')
+    _('Schema')
+    _('Service Description')
+    _('Statistical')
+    _('Syntax Encoding Scheme')
+    _('Taxonomy')
+    _('Thesaurus')
 
-    #Status
-    _("Completed")
-    _("Deprecated")
-    _("Under Development")
-    _("Withdrawn")
+    # status
+    _('Completed')
+    _('Deprecated')
+    _('Under Development')
+    _('Withdrawn')
 
-    #Accural Periodicy
-    _("Never")
-    _("Discontinued")
-    _("Annual")
-    _("Quarterly")
-    _("Monthly")
-    _("Other - please specify")
+    # accural Periodicy
+    _('Never')
+    _('Discontinued')
+    _('Annual')
+    _('Quarterly')
+    _('Monthly')
+    _('Other - please specify')
 
-    #Interoperability level
-    _("Never")
-    _("Discontinued")
-    _("Annual")
-    _("Quarterly")
-    _("Monthly")
-    _("Other - please specify")
+    # interoperability level
+    _('Never')
+    _('Discontinued')
+    _('Annual')
+    _('Quarterly')
+    _('Monthly')
+    _('Other - please specify')
 
+    # header
+    _('Legal notice')
+    _('Contact')
+    _('Search')
+    _('Home')
+    _('Data')
+    _('Applications')
+    _('Linked Data')
+    _('Social Media')
+    _('Add a dataset')
+    _('Publishers')
+    _('The EU Open Data Hub')
+    _('The Open Data Hub of the European Union')
+    _('European Commission')
+    _('Open Data Portal')
+
+    # footer
+    _('Last update')
+    _('Top')
+
+    # home
+    _('Find data')
+    _('Top publishers')
+    _('Recent updates')
+
+    # dataset
+    _('Publisher')
+    _('This dataset has been marked as deleted')
+
+    # resource
+    _('View')
+    _('API Endpoint')
+    _('Download')
+    _('Data API')
+    _('Preview')
+
+    # search
+    _('Tags')
+    _('Resource formats')
+    _('Groups')
+
+    # publishers
+    _('List Publishers')
+    _('Add a Publisher')
+    _('Join')
+    _('Image URL')
+    _('Parent Publisher')
+    _('Please choose a publisher')
+    _('Extras')
+    _('Add')
+    _('Key')
+    _('Value')
+    _('Manage users')
