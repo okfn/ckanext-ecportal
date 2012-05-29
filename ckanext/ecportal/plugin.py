b35a44077bae99b3498aebcd16e1676823e4534a
John Glover <j@johnglover.net>
2012-05-29 13:40:30 +0100

    Tidy up plugin.py (use plugin toolkit)

--- ckanext/ecportal/plugin.py
+++ ckanext/ecportal/plugin.py
@@ -1,42 +1,16 @@
-import os
-from ckan.plugins import implements, SingletonPlugin, IConfigurable,\
-    IConfigurer
-import ckanext.ecportal
-
-from logging import getLogger
-log = getLogger(__name__)
+import ckan.plugins as p
 
 
-def configure_template_directory(config, relative_path):
-    configure_served_directory(config, relative_path, 'extra_template_paths')
-
-
-def configure_public_directory(config, relative_path):
-    configure_served_directory(config, relative_path, 'extra_public_paths')
-
-
-def configure_served_directory(config, relative_path, config_var):
-    'Configure serving of public/template directories.'
-    assert config_var in ('extra_template_paths', 'extra_public_paths')
-    this_dir = os.path.dirname(ckanext.ecportal.__file__)
-    absolute_path = os.path.join(this_dir, relative_path)
-    if absolute_path not in config.get(config_var, ''):
-        if config.get(config_var):
-            config[config_var] += ',' + absolute_path
-        else:
-            config[config_var] = absolute_path
-
-
-class ECPortalPlugin(SingletonPlugin):
-    implements(IConfigurable)
-    implements(IConfigurer)
+class ECPortalPlugin(p.SingletonPlugin):
+    p.implements(p.IConfigurable)
+    p.implements(p.IConfigurer)
 
     def configure(self, config):
         self.site_url = config.get('ckan.site_url')
 
     def update_config(self, config):
-        configure_template_directory(config, 'templates')
-        configure_public_directory(config, 'public')
+        p.toolkit.add_template_directory(config, 'templates')
+        p.toolkit.add_public_directory(config, 'public')
 
         # ECPortal should use group auth
         config['ckan.auth.profile'] = 'publisher'
