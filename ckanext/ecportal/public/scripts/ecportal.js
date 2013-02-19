c93de700d52c869bd6b46ba39dab43f54055017b
John Martin <me@johnmart.in>
2013-02-19 16:32:03 +0000

    [#2] Re-added language dropdown

--- ckanext/ecportal/public/scripts/ecportal.js
+++ ckanext/ecportal/public/scripts/ecportal.js
@@ -1,5 +1,28 @@
 (function ($) {
-    $(document).ready(function () {
+	$(function () {
+		var arrow_url = CKAN.SITE_URL_NO_LOCALE + '/images/arrow_{0}.gif';
+		var dropdown = $('#language-selector ul');
+		var dropdown_image = $('#language-selector .root img');
+		dropdown
+			.bind('mouseleave', function() {
+				_dropdown(false);
+			})
+			.parent()
+			.bind('click', function() {
+				_dropdown(true);
+			});
+		function _dropdown($show) {
+			if ($show) {
+				dropdown.show();
+				dropdown_image.attr('src', arrow_url.replace('{0}', 'up'));
+			} else {
+				dropdown.hide();
+				dropdown_image.attr('src', arrow_url.replace('{0}', 'down'));
+			}
+		}
+	});
+}(jQuery));
+
 		/* START language selector functionality
 		 */
 		$('ul.language-dd-selector').click(
@@ -18,8 +41,6 @@
 			function (){
 				// hide list
 				$('ul.language-dd-selector li:gt(0)').hide();
-                $('ul.language-dd-selector li img').attr('src', CKAN.SITE_URL_NO_LOCALE + '/images/arrow_down.gif');
+								$('ul.language-dd-selector li img').attr('src', CKAN.SITE_URL_NO_LOCALE + '/images/arrow_down.gif');
 				})
 		/* END language selector functionality */
-    });
-}(jQuery));
