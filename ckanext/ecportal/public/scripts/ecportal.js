5c300f63feb47f98255cbcba1af95d514534c57a
John Martin <me@johnmart.in>
2013-02-26 14:15:39 +0000

    [#2] Tweaks to the theme

--- ckanext/ecportal/public/scripts/ecportal.js
+++ ckanext/ecportal/public/scripts/ecportal.js
@@ -13,34 +13,12 @@
 			});
 		function _dropdown($show) {
 			if ($show) {
-				dropdown.show();
+				dropdown.fadeIn();
 				dropdown_image.attr('src', arrow_url.replace('{0}', 'up'));
 			} else {
-				dropdown.hide();
+				dropdown.slideUp();
 				dropdown_image.attr('src', arrow_url.replace('{0}', 'down'));
 			}
 		}
 	});
 }(jQuery));
-
-		/* START language selector functionality
-		 */
-		$('ul.language-dd-selector').click(
-			function (){
-				if ($('ul.language-dd-selector li:visible').length == 1){
-					// show list
-					$('ul.language-dd-selector li').show();
-					$('ul.language-dd-selector li img').attr('src', CKAN.SITE_URL_NO_LOCALE + '/images/arrow_up.gif');
-				} else {
-					// hide list
-					$('ul.language-dd-selector li:gt(0)').hide();
-					$('ul.language-dd-selector li img').attr('src', CKAN.SITE_URL_NO_LOCALE + '/images/arrow_down.gif');
-				}
-				})
-		$('ul.language-dd-selector').mouseleave(
-			function (){
-				// hide list
-				$('ul.language-dd-selector li:gt(0)').hide();
-								$('ul.language-dd-selector li img').attr('src', CKAN.SITE_URL_NO_LOCALE + '/images/arrow_down.gif');
-				})
-		/* END language selector functionality */
